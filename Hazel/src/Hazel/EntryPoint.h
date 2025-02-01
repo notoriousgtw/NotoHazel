73d905491c34dec7df1feaa46a64d8d77ebdb58e
NotoriousGTW <notoriousgtw@gmail.com>
2025-01-31 19:22:03 -0500

    Basic setup, entrypoint, logger

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Application.h"
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** arc)
+{
+	Hazel::Log::Init();
+	HZ_CORE_TRACE("Initialized Logger!");
+	HZ_INFO("Hello!");
+
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif // HZ_PLATFORM_WINDOWS
